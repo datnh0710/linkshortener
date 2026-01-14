f45b96f64ea393305f4f574a11cba29349f73974
hoangdatnguyen <54076449+datnh0710@users.noreply.github.com>
2026-01-13 21:14:30 -0600

    Adjust the Middleware for sign in and sign out

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, request) => {
+    if (!isPublicRoute(request)) {
+        await auth.protect();
+    }
+}, {
+    signInUrl: "/sign-in",
+    signUpUrl: "/sign-up",
+    afterSignInUrl: "/dashboard",
+    afterSignUpUrl: "/dashboard",
+});
 
 export const config = {
     matcher: [
